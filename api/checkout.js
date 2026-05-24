ffe9eeab35f9229e003300342e59ee550afc6106
nukkeninprod@gmail.com <nukkeninprod@gmail.com>
2026-05-24 22:10:47 +0200

    Enable Stripe automatic payment methods

--- api/checkout.js
+++ api/checkout.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
 
     const session = await stripe.checkout.sessions.create({
       mode: 'payment',
-      payment_method_types: ['card'],
+      automatic_payment_methods: { enabled: true },
       line_items: [
         {
           price_data: {
